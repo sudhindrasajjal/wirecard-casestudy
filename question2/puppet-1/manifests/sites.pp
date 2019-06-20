92eb748c655cb367213f763a24856b1f9ad84c08
sudhindra.s <sudhindra.s@BMMDVSUDHINDRA.local>
2019-06-20 19:22:47 +0530

    add readme for question 2

---
+++ question2/puppet-1/manifests/sites.pp
@@ -0,0 +1,40 @@
+class { 'java':
+  package => 'java-1.8.0-openjdk-devel',
+}
+
+tomcat::install { '/opt/tomcat-instances':
+  source_url => 'http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.21/bin/apache-tomcat-9.0.21.tar.gz'
+}
+
+tomcat::instance { 'tomcatA':
+  catalina_home => '/opt/tomcat-instances',
+  catalina_base => '/opt/tomcat-instances/tomcatA',
+  user => 'tomcat9',
+  group => 'tomcat9',
+}
+
+tomcat::instance { 'tomcatB':
+  catalina_home => '/opt/tomcat-instances',
+  catalina_base => '/opt/tomcat-instances/tomcatB',
+}
+
+tomcat::config::server { 'tomcatA':
+  catalina_base => '/opt/tomcat-instances/tomcatA',
+  port          => '8080',
+}
+
+tomcat::config::server { 'tomcatB':
+  catalina_base => '/opt/tomcat-instances/tomcatB',
+  port          => '18080',
+}
+
+tomcat::war { 'hello_world.war':
+  catalina_base => '/opt/tomcat-instances/tomcatA',
+  war_source    => '/opt/tomcat-instances/webapps/hello_world.war',
+}
+
+tomcat::war { 'hello_world.war':
+  catalina_base => '/opt/tomcat-instances/tomcatA',
+  war_source    => '/opt/tomcat-instances/webapps/hello_world1.war',
+}
+
