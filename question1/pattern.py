24fcc6c90bb6a1b0f33a7b1d8de24b17d4daf089
Sudhindra Sajjal <sudhindrasajjal@gmail.com>
2019-06-21 15:12:53 +0530

    Update pattern.py

--- question1/pattern.py
+++ question1/pattern.py
@@ -3,6 +3,7 @@ import math
 print 'Enter number of rows'
 r = raw_input()
 
+# Function to compute the nth number of the the Beatty Sequence. Source: http://oeis.org/A054385
 def compute_row_count(n):
 	return int(math.floor(n*math.exp(1) / (math.exp(1) - 1)))
 
